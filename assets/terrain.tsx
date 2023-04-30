52e6c1067616f0db96b01d46318159eaec93922b
Rémi Vanicat <vanicat@debian.org>
2023-04-30 12:52:03 +0200

    Updating asset

--- assets/terrain.tsx
+++ assets/terrain.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="terrain" tilewidth="32" tileheight="32" tilecount="108" columns="12">
- <image source="tile1.png" width="384" height="288"/>
+<tileset version="1.10" tiledversion="1.10.1" name="terrain" tilewidth="32" tileheight="32" tilecount="240" columns="18">
+ <image source="tile1.png" width="576" height="320"/>
 </tileset>
