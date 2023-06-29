e96696f15bfb4b9766426fc611046560c628dde4
Rémi Vanicat <vanicat@debian.org>
2023-06-29 13:30:43 +0200

    Change the build command

--- build.ps1
+++ build.ps1
@@ -1,4 +1,22 @@
-Remove-Item .\app.dist\assets
+Remove-Item .\build\assets
+
 git pull 
-python.exe -m nuitka --mingw64 .\app.py
-Copy-Item -r .\assets\ .\app.dist\
+.\ludum\Scripts\python.exe -m nuitka --follow-imports .\app.py
+
+mkdir .\build\assets
+mkdir .\build\assets\music
+
+Copy-Item "assets\boat.tmj" .\build\assets
+Copy-Item "assets\boat.png" .\build\assets
+Copy-Item "assets\boat-port.png" .\build\assets
+Copy-Item "assets\carte1.tmj" .\build\assets
+Copy-Item "assets\fond.png" .\build\assets
+Copy-Item "assets\materiel.json" .\build\assets
+Copy-Item "assets\roue2.png" .\build\assets
+Copy-Item "assets\roue2-direction.png" .\build\assets
+Copy-Item "assets\terrain.tsx" .\build\assets
+Copy-Item "assets\tile1.png" .\build\assets
+Copy-Item .\assets\music\music-wave.mp3 .\build\assets\music
+
+#Copy-Item -r .\assets .\build
+Copy-Item app.exe .\build
