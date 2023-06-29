f677b54b3fad0852bf07c5f7eedab7e02f8eb457
Rémi Vanicat <vanicat@debian.org>
2023-06-29 13:30:59 +0200

    stuff..

---
+++ build/app-test.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'app.exe' with Python DLL being found.
+set PATH="%~dp0python";%PATH%
+set PYTHONHOME="%~dp0"
+"python.exe" app.py
---
+++ build/boat.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'app.exe' with Python DLL being found.
+set PATH="%~dp0python";%PATH%
+set PYTHONHOME="%~dp0"
+"%~dp0.\app.exe" %*
